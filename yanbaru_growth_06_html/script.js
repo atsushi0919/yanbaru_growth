141a36d5823c584f7843fedc7de4fbb7b7daa03d
Atsushi Hatakeyama <atsushi@AtsushinoMacBook-Pro.local>
2021-05-19 22:30:44 +0900

    update/yanbaru_growth_06

--- yanbaru_growth_06_html/script.js
+++ yanbaru_growth_06_html/script.js
@@ -1,8 +1,8 @@
 // コンテキスト変数を宣言
-let context;
+let context
 
 // 時, 分, 秒 の変数を宣言
-let h, m, s;
+let h, m, s
 
 // idで要素を取得する関数
 // function gobj(id) {
@@ -14,102 +14,102 @@ function init() {
   // gobj 関数使用
   // context = gobj('clock').getContext('2d');
 
-  context = document.getElementById('clock').getContext('2d');
+  context = document.getElementById('clock').getContext('2d')
 
   // タイマー処理: 1000ms毎に tick 関数を繰り返す
-  setInterval(tick, 1000);
+  setInterval(tick, 1000)
 }
 
 function tick() {
   // 現在時刻を取得
-  var now = new Date();
+  var now = new Date()
 
   // 現在時刻 now から 12時間表記の時刻の時間を取得
-  h = now.getHours() % 12;
+  h = now.getHours() % 12
   // 現在時刻 now から分を取得
-  m = now.getMinutes();
+  m = now.getMinutes()
   // 現在時刻 now から秒を取得
-  s = now.getSeconds();
+  s = now.getSeconds()
 
   // gobj 関数使用
   // gobj('time').textContent = now.toTimeString();
 
   // id="time" の要素に現在時刻を文字列で表示する
-  document.getElementById('time').textContent = now.toTimeString();
+  document.getElementById('time').textContent = now.toTimeString()
 
   // paint 関数を実行
-  paint();
+  paint()
 }
 
 function drawHand(rotation, length, width, color) {
   // 現在の描画スタイルを保存する
-  context.save();
+  context.save()
   // 太さを設定
-  context.lineWidth = width;
+  context.lineWidth = width
   // 色を設定
-  context.strokeStyle = color;
+  context.strokeStyle = color
   // (x:150,y:150)に移動（中心）
-  context.translate(150, 150);
+  context.translate(150, 150)
   // 指定角度に回転
-  context.rotate(rotation);
+  context.rotate(rotation)
   // 現在のパスをリセット
-  context.beginPath();
+  context.beginPath()
   // 新しいサブパスの開始点を指定(x:0,y:0)
   // 座標移動後なので中心
-  context.moveTo(0, 0);
+  context.moveTo(0, 0)
   // 中心から 長さlengthの線を引く
-  context.lineTo(0, -length);
+  context.lineTo(0, -length)
   // 現在の線スタイル(太さと色)で輪郭表示
-  context.stroke();
+  context.stroke()
   // 保存した描画スタイルに戻す
-  context.restore();
+  context.restore()
 }
 
 function paint() {
   // 左上(x:0,y:0)、右下(x:300,y:300)の正方形範囲をクリアする
   // 原点は(x:0,y:0) 右: +x , 下: +y
-  context.clearRect(0, 0, 300, 300);
+  context.clearRect(0, 0, 300, 300)
   // 現在の描画スタイルを保存する
-  context.save();
+  context.save()
 
   // x+150, y+150 に移動する（中心）
-  context.translate(150, 150);
+  context.translate(150, 150)
   // 描画色を黒にする
-  context.strokeStyle = 'black';
+  context.strokeStyle = 'black'
   // 1分=6° を 弧度法に変換
-  var pitch = (Math.PI * 2) / 60;
+  var pitch = (Math.PI * 2) / 60
 
   // 0分から59分までを繰り返す
   for (var i = 0; i < 60; i++) {
     // if (i === 6) {
-    //   debugger;
+    //   debugger
     // }
 
     // 現在のパスをリセット
-    context.beginPath();
+    context.beginPath()
     // i 分が 5で割り切れるときは 太さ3,
     // 割り切れないときは 太さ1 を設定
-    context.lineWidth = i % 5 == 0 ? 3 : 1;
+    context.lineWidth = i % 5 == 0 ? 3 : 1
     // 新しいサブパスの開始点を指定(x:0,y:-120)
-    context.moveTo(0, -120);
+    context.moveTo(0, -120)
     // 直前の開始点(x:0,y:-120)から(x:0,y-140)まで線を引く
-    context.lineTo(0, -140);
+    context.lineTo(0, -140)
     // 現在の線スタイル(太さと色)で輪郭表示
-    context.stroke();
+    context.stroke()
     // 座標系を時計回りに pitch分 回転させる
-    context.rotate(pitch);
+    context.rotate(pitch)
   }
   // 保存した描画スタイルに戻す
-  context.restore();
+  context.restore()
 
   // 現在時刻 h,m,s の時計上の角度を求める
-  let radH = ((Math.PI * 2) / 12) * h + ((Math.PI * 2) / 12) * (m / 60);
-  let radM = ((Math.PI * 2) / 60) * m;
-  let radS = ((Math.PI * 2) / 60) * s;
+  let radH = ((Math.PI * 2) / 12) * h + ((Math.PI * 2) / 12) * (m / 60)
+  let radM = ((Math.PI * 2) / 60) * m
+  let radS = ((Math.PI * 2) / 60) * s
 
   // 時針, 分針, 秒針 を描画する
   // drawHenad(角度, 長さ, 太さ, 色)
-  drawHand(radH, 80, 6, 'blue');
-  drawHand(radM, 120, 4, 'blue');
-  drawHand(radS, 140, 2, 'red');
+  drawHand(radH, 80, 6, 'blue')
+  drawHand(radM, 120, 4, 'blue')
+  drawHand(radS, 140, 2, 'red')
 }

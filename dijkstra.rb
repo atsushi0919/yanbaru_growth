cec2047c6b8bed039a70ae70327cd67cfb7b6e48
Atsushi Hatakeyama <hatake0919@yahoo.co.jp>
2022-03-10 22:56:28 +0900

    ダイクストラ法サンプルコード作成

---
+++ dijkstra.rb
@@ -0,0 +1,50 @@
+# ダイクストラ法(Dijkstra's Algorithm)
+
+# 隣接リスト
+ADJACENCY_LIST = [
+  [[1, 4], [2, 7], [5, 9]],
+  [[0, 4], [2, 2], [3, 6]],
+  [[0, 7], [1, 2], [3, 3], [5, 5]],
+  [[1, 6], [2, 3], [4, 5]],
+  [[3, 5], [5, 7]],
+  [[0, 9], [4, 7]],
+]
+
+NODE = 6
+START = 0
+
+# 各ノードまでの最小コストを nil で初期化
+min_costs = Array.new(NODE)
+
+# スタート地点の情報[0: 運賃, 0: 駅番号]で routes を初期化
+search_list = [[0, 0]]
+
+# search_list が空になるまで繰り返す
+while not search_list.empty?
+  # 最少コストの要素(ノード)を取り出す
+  search_list.sort!
+  # cost: 取り出したノードの最少コスト
+  # node: 取り出したノードの番号
+  cost, node = search_list.shift
+
+  # 現在のノード最少コストが確定済みならスキップ
+  next if not min_costs[node].nil?
+
+  # 現在のノードを最少コストで確定させる
+  min_costs[node] = cost
+
+  # 全ノードの最少コストが確定したら終了
+  break if not min_costs.any?(nil)
+
+  # 隣接ノードを順番に調べる
+  ADJACENCY_LIST[node].each do |next_node, next_cost|
+    # 隣接ノードの最少コストが確定済みならスキップ
+    next if not min_costs[next_node].nil?
+    # search_list に隣接ノードまでのcostとノード番号を追加
+    search_list << [cost + next_cost, next_node]
+  end
+end
+
+min_costs.each_with_index do |cost, idx|
+  puts "#{START} から #{idx} までの最少コストは #{cost.to_s.rjust(2)} です"
+end
